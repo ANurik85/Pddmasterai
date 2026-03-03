50048130aef3eddab06306157d06f252e0d971af
Nurik <Yanzip@yandex.ru>
2026-03-03 13:52:43 +0500

    fix: update github workflow and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: "/Pddmasterai/",
     appType: 'spa',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -52,7 +53,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
